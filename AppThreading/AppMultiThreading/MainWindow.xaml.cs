c3c123508b4c8c48b9a89fa2c35ad0818ec7a228
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:08:15 +0200

    creazione bottone con azione pesante

--- AppThreading/AppMultiThreading/MainWindow.xaml.cs
+++ AppThreading/AppMultiThreading/MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace AppMultiThreading
         {
             InitializeComponent();
         }
+
+        private void Btn_Task_Click(object sender, RoutedEventArgs e)
+        {
+            //DoWork();
+            
+        }
+
+        private void DoWork()
+        {
+            //azione che crusha tutto perchè richiede parecchi calcoli(se fatto in un unico thread)
+            for (int i = 1; i < 10000; i++)
+            {
+                for (int j = 1; j < 10000; j++)
+                {
+
+                }
+            }
+            Lbl_Result.Content = "Finito";
+        }
     }
 }
